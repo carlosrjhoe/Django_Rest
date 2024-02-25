c6c302d633d8b984e70fb03d7f53d8d27e0740dc
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-02-24 21:05:54 -0300

    Curso Django Rest: Configurando Urls, criando e registrando uma rota padrão para alunos e cursos

--- Alura_Django_Rest/setup/urls.py
+++ Alura_Django_Rest/setup/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from escola import views
+from django.urls import path, include
+from rest_framework import routers
+from escola.views import AlunoViewSet, CursoViewSet
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunoViewSet, basename='Alunos')
+router.register('cursos', CursoViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.alunos),
+    path('', include(router.urls)),
 ]
