cf340bb171db55b5f16ff8b0dfcd4f9461844ce2
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-02-24 21:40:20 -0300

    Curso Django Rest: Alterei quantidade de caracteres do atributo Aluno.nome

--- Alura_Django_Rest/escola/models.py
+++ Alura_Django_Rest/escola/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Aluno(models.Model):
-    nome = models.CharField(max_length=30)
+    nome = models.CharField(max_length=255)
     rg = models.CharField(max_length=9)
     cpf = models.CharField(max_length=11)
     data_nascimento = models.DateField()
