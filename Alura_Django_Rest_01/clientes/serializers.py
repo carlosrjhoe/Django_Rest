103e5818fe5183f7009b55d69f4e256289b4547a
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-02-29 20:57:09 -0300

    Curso Django Rest: Criando validações para os compos do model

--- Alura_Django_Rest_01/clientes/serializers.py
+++ Alura_Django_Rest_01/clientes/serializers.py
@@ -10,3 +10,19 @@ class ClienteSerializer(serializers.ModelSerializer):
         if len(cpf) != 11:
             raise serializers.ValidationError('O cpf deve ter 11 digitos')
         return cpf
+
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('O nome não pode ter números')
+        return nome
+
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError('O rg deve ter 9 digitos')
+        return rg
+
+    def validate_celular(self, celular):
+        if len(celular) != 11:
+            raise serializers.ValidationError('O celular deve ter 11 digitos')
+        return celular
+            
