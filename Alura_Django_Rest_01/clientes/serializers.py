88c48f3eb0156e403278ac3dcf334a630a313445
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-02-29 21:29:20 -0300

    Curso Django Rest: Melhorando validações como realmente de ser

--- Alura_Django_Rest_01/clientes/serializers.py
+++ Alura_Django_Rest_01/clientes/serializers.py
@@ -1,28 +1,20 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError('O cpf deve ter 11 digitos')
-        return cpf
-
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError('O nome não pode ter números')
-        return nome
-
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError('O rg deve ter 9 digitos')
-        return rg
-
-    def validate_celular(self, celular):
-        if len(celular) != 11:
-            raise serializers.ValidationError('O celular deve ter 11 digitos')
-        return celular
+    def validate(self, data):
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': 'O nome não pode ter números'})
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': 'O cpf deve ter 11 digitos'})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': 'O rg deve ter 9 digitos'})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': 'O celular deve ter 11 digitos'})
+        return data
             
