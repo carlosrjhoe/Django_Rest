c91f4a3cccdf12fb8cfeef1b0950684472151fec
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-08 02:36:34 -0300

    Curso Django Rest-Verssionamento e etc: Configurando API para compartilhamento de recursos entre origens diferentes

--- Alura_Django_Rest/setup/settings.py
+++ Alura_Django_Rest/setup/settings.py
@@ -39,11 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'escola',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,3 +147,7 @@ REST_FRAMEWORK = {
         'user': '1000/day'
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
