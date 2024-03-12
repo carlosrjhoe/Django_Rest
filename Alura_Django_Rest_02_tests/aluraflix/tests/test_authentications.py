65942bf5a957dd365351bf7b6b8ebf9fbbf05609
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-11 21:22:28 -0300

    Curso API com Django 3 com testes e documentação: Criando test para verificar authenticação com username e password incorretos

--- Alura_Django_Rest_02_tests/aluraflix/tests/test_authentications.py
+++ Alura_Django_Rest_02_tests/aluraflix/tests/test_authentications.py
@@ -8,14 +8,24 @@ class AuthenticationsUserTestCase(APITestCase):
 
     def setUp(self) -> None:
         self.list_url = reverse('programas-list')
-        self.user = User.objects.create_user('c3po', password='admin')
+        self.user = User.objects.create_user('admin', password='admin')
 
     def test_authenticacao_user_com_credenciais_corretas(self):
         """Test que verificar authenticação de user com credenciais corretas"""
-        user = authenticate(username='c3po', password='admin')
+        user = authenticate(username='admin', password='admin')
         self.assertTrue((user is not None) and user.is_authenticated)
 
     def test_requisicao_GET_nao_autorizada(self):
         """Test que verifica uma requisição GET não autorizada"""
         response = self.client.get(self.list_url)
         self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticacao_de_user_com_username_incorreto(self):
+        """Test que verifica authenticação de user com username incorreto"""
+        user = authenticate(username='admi', password='admin')
+        self.assertFalse((user is not None) and user.is_authenticated)
+
+    def test_authenticacao_de_user_com_password_incorreto(self):
+        """Test que verifica authenticação de user com password incorreto"""
+        user = authenticate(username='admin', password='administracao')
+        self.assertFalse((user is not None) and user.is_authenticated)
