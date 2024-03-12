09a400d93d13dc4bc0cb947d63bdf5244f7e9acc
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-03-11 21:17:12 -0300

    Curso API com Django 3 com testes e documentação: Criando test para para verificar requisição GET não autorizada

--- Alura_Django_Rest_02_tests/aluraflix/tests/test_authentications.py
+++ Alura_Django_Rest_02_tests/aluraflix/tests/test_authentications.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.contrib.auth import authenticate
+from django.urls import reverse
+from rest_framework import status
 
 class AuthenticationsUserTestCase(APITestCase):
 
     def setUp(self) -> None:
+        self.list_url = reverse('programas-list')
         self.user = User.objects.create_user('c3po', password='admin')
 
     def test_authenticacao_user_com_credenciais_corretas(self):
         """Test que verificar authenticação de user com credenciais corretas"""
         user = authenticate(username='c3po', password='admin')
         self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_requisicao_GET_nao_autorizada(self):
+        """Test que verifica uma requisição GET não autorizada"""
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
